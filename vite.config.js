86f01200595acf2e759c4b3a7e1dd79a40b53fe4
agustinglaiel <agustinglaiel@gmail.com>
2025-07-25 11:24:20 -0300

    hosts

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'testing.gopredi.com',
+      'http://testing.gopredi.com',
+      'https://testing.gopredi.com',
+    ]
+  }
 });
