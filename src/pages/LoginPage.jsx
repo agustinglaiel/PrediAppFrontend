da556765b3929d3a7a981b753a2f0c1ab1978efa
agustinglaiel <agustinglaiel@gmail.com>
2025-07-31 08:17:09 -0300

    vuelta atras de estilo login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,18 +8,7 @@ import Threads from "../components/Threads";
 
 const LoginPage = () => {
   return (
-    <div className="relative w-full h-screen bg-black overflow-hidden">
-      {/* Fondo WebGL centrado */}
-      <div className="absolute inset-0 flex justify-center items-center">
-        {/* Si quieres que ocupe todo el ancho/alto, mantiene w-full h-full */}
-        {/* si prefieres un tamaño fijo, cámbialo aquí: e.g. w-3/4 h-3/4 */}
-        <Threads
-          amplitude={3}
-          distance={0}
-          enableMouseInteraction={true}
-        />
-      </div>
-
+    <div className="relative w-full h-screen bg-white overflow-hidden">
       {/* Contenido superpuesto */}
       <div className="absolute inset-0 flex flex-col">
         <Header />
@@ -29,9 +18,9 @@ const LoginPage = () => {
           <LoginForm />
         </main>
 
-        {/* <footer className="bg-gray-200 text-gray-700 text-center py-3 text-sm">
+        <footer className="bg-gray-200 text-gray-700 text-center py-3 text-sm">
           <p>© 2025 PrediApp</p>
-        </footer> */}
+        </footer>
       </div>
     </div>
   );
